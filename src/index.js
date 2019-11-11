052911ca78f3fd3389864ffaa017cb9960e07e71
github username <github email address>
2019-11-11 15:38:48 -0800

    update to new curriculum

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      let ceoImageContainer = document.getElementById('dog-image-container');
+
+      for (const element of json.message) {
+        let img = document.createElement('img');
+        img.src = element;
+        ceoImageContainer.appendChild(img);
+      }
+    });
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        breedContainer = document.getElementById('dog-breeds');
+        breeds = Object.keys(json.message)
+
+        for (const element of breeds) {
+          let li = document.createElement('li');
+          li.innerHTML = element;
+          breedContainer.appendChild(li);
+        }
+      });
+});
